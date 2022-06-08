c162509cf58e1125ccf0877d9c96c2e2c2d1bce7
Davi Albino <negroalbino_@hotmail.com>
2022-06-08 13:07:28 -0300

    Adicionando requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,21 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se ao utilizar a função com o argumento "computador" a função fetch irá usar como referência a URL correta', () => {
+    fetchProducts('computador');
+
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  } )
+
+  it('Teste se ao utilizar a função com o argumento "computador" retorna um objeto exatamente igual ao objeto computadorSearch', async () => {
+    const productsObject = await fetchProducts('computador');
+    
+    expect(productsObject).toStrictEqual(computadorSearch)
+  } )
+
+  it('Testar se ao chamar a função fetchProducts sem argumento, retornar um erro escrito "You must provide an url"', async () => {
+    const productsNoArgument = await fetchProducts();
+
+    expect(productsNoArgument).toEqual(new Error ('You must provide an url'));
+  })
 });
